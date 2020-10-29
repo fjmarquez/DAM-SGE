0d824fd01c76cfd37713a3d7939e3b144d4422c9
Francisco José Márquez Pulido <fjmarquez@info2>
2020-10-29 13:39:01 +0100

    ejercicio 1 unidad 8 terminado

---
+++ 08ASPNETMVCDatosControlador/08ASPNETMVCDatosControlador01/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _08ASPNETMVCDatosControlador01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Saludo(String nombre, String apellidos)
+        {
+            ViewBag.saludo = "Hola " + nombre + " " + apellidos;
+            return View();
+        }
+    }
+}
---
+++ 08ASPNETMVCDatosControlador/08ASPNETMVCDatosControlador02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_08ASPNETMVCDatosControlador02.MvcApplication" Language="C#" %>
