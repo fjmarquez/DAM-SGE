ae7f2f02b35c2d7e3b9b4b0a0dac137a5d4ca195
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2020-10-29 16:08:02 +0100

    Añadido ejercicio 7

---
+++ 07ASPDatos/07ASPDatosE1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using _07ASPDatosE1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _07ASPDatosE1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string saludo;
+            string fechaActual = DateTime.Now.ToString();
+
+            clsPersona p = new clsPersona();
+            p.IDPersona = 1;
+            p.nombre = "Francisco";
+            p.apellidos = "Marquez Pulido";
+            p.direccion = "Huelva";
+            p.telefono = "123456789";
+
+            if (DateTime.Now.Hour > 12 && DateTime.Now.Hour < 20)
+            {
+                saludo = "Buenas tardes";
+            }
+            else if (DateTime.Now.Hour < 12 && DateTime.Now.Hour > 5)
+            {
+                saludo = "Buenos dias";
+            }
+            else
+            {
+                saludo = "Buenas noches";
+            }
+
+            ViewData["saludo"] = saludo;
+            ViewBag.fecha = fechaActual;
+
+            return View(p);
+        }
+
+        public ActionResult listadoPersonas()
+        {
+            List<clsPersona> listaPersonas = new List<clsPersona>();
+
+
+            return View();
+        }
+    }
+}
---
+++ 07ASPDatos/07ASPDatosE1/Models/clsPersona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _07ASPDatosE1.Models
+{
+    public class clsPersona
+    {
+
+        public int IDPersona { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+
+    }
+}
