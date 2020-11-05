639c8129bf3cc89f6613c75b4c76ad26a4752fe6
Francisco José Márquez Pulido <fjmarquez@info2>
2020-11-05 13:39:12 +0100

    Ejercicio 3 unidad 8 empezado

---
+++ 08ASPNETMVCDatosControlador/08ASPNETMVCDatosControlador03/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using _08ASPNETMVCDatosControlador03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _08ASPNETMVCDatosControlador03.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            DateTime d = new DateTime().Date;
+
+            clsPersona persona1 = new clsPersona(2, "José", "Márquez", d, "Villarrasa 1", "555555555");
+            
+            return View(persona1);
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
