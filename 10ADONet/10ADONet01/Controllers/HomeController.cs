0b512f9ed54ed465394fb4d4f15f8117ace098a2
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2020-11-12 20:11:58 +0100

    Ejercicio 1 unidad 10

---
+++ 10ADONet/10ADONet01/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _10ADONet01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Index")]
+        public ActionResult IndexPost()
+        {
+            SqlConnection sqlc = new SqlConnection();
+
+            try
+            {
+                sqlc.ConnectionString = "server=fjmarquez.database.windows.net;database=fjmarquez;uid=fjmarquez;pwd=Mitesoro1.";
+                sqlc.Open();
+                ViewBag.ConexionBD = "Conexion abierta correctamente";
+            }
+            catch(SqlException e)
+            {
+                ViewBag.ConexionBD = "Error";
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ 10ADONet/10ADONet01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_10ADONet01.MvcApplication" Language="C#" %>
