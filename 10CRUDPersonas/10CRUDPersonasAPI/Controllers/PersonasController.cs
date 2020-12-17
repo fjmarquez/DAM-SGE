23a6febc1e51a330e8c68a95390935ec98e79538
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2020-12-17 22:45:09 +0100

    API empezada

---
+++ 10CRUDPersonas/10CRUDPersonasAPI/Controllers/PersonasController.cs
@@ -0,0 +1,41 @@
+﻿using _10CRUDPersonasBL.Listados;
+using _10CRUDPersonasEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _10CRUDPersonasAPI.Controllers
+{
+    public class PersonasController : ApiController
+    {
+        // GET: api/Personas
+        public IEnumerable<clsPersona> Get()
+        {
+            return new listadoPersonasBL().getListadoPersonas();
+        }
+
+        // GET: api/Personas/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Personas
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Personas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Personas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ 10CRUDPersonas/10CRUDPersonasUI/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_10CRUDPersonasUI.MvcApplication" Language="C#" %>
