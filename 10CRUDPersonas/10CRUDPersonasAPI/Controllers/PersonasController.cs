8dfa4b7a74326f096bfc2206c30583ed7c18cd6f
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2021-01-12 17:50:40 +0100

    API personas terminada

--- 10CRUDPersonas/10CRUDPersonasAPI/Controllers/PersonasController.cs
+++ 10CRUDPersonas/10CRUDPersonasAPI/Controllers/PersonasController.cs
@@ -1,4 +1,5 @@
-﻿using _10CRUDPersonasBL.Listados;
+﻿using _10CRUDPersonasBL.Handlers;
+using _10CRUDPersonasBL.Listados;
 using _10CRUDPersonasEntities;
 using System;
 using System.Collections.Generic;
@@ -15,31 +16,138 @@ namespace _10CRUDPersonasAPI.Controllers
         public IEnumerable<clsPersona> Get()
         {
             listadoPersonasBL handler = new listadoPersonasBL();
+            IEnumerable<clsPersona> lista = new clsPersona[]{};
 
-            return handler.getListadoPersonas();
+            try
+            {
+                lista = handler.getListadoPersonas();
+                
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+
+            if (lista.Count() > 0)
+            {
+                
+                throw new HttpResponseException(HttpStatusCode.OK);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            return lista;
         }
 
         // GET: api/Personas/5
         public clsPersona Get(int id)
         {
             listadoPersonasBL handler = new listadoPersonasBL();
+            clsPersona p = new clsPersona();
 
-            return handler.getPersonaPorID(id);
+            try
+            {
+                p = handler.getPersonaPorID(id);
+                
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+
+            if(p.Id != 0)
+            {
+                throw new HttpResponseException(HttpStatusCode.OK);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            return p;
         }
 
         // POST: api/Personas
-        public void Post([FromBody]string value)
+        public void Post(clsPersona value)
         {
+            clsHandlerPersonaBL handler = new clsHandlerPersonaBL();
+            int state = 0;
+
+            try
+            {
+                state = handler.createPersonaBL(value);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+
+            if(state == 0)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.OK);
+            }
+
         }
 
         // PUT: api/Personas/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(clsPersona value)
         {
+            clsHandlerPersonaBL handler = new clsHandlerPersonaBL();
+            int state = 0;
+
+            try
+            {
+                state = handler.updatePersonaBL(value);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+
+            if(state == 0)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.Created);
+            }
+            
         }
 
         // DELETE: api/Personas/5
         public void Delete(int id)
         {
+            clsHandlerPersonaBL handler = new clsHandlerPersonaBL();
+            int state = 0;
+
+            try
+            {
+                state = handler.deletePersonaBL(id);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+
+            if(state == 0)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                throw new HttpResponseException(HttpStatusCode.NoContent);
+            }
+
+            
+
+
         }
     }
 }
