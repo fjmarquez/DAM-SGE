56a6dddf5a9fb58456f113455aebc5b70141429c
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2021-01-14 23:13:25 +0100

    Chat con SignalR

---
+++ 11SignalRChat/11SignalRChat/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_11SignalRChat.Startup))]
+
+namespace _11SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+
+    }
+}
