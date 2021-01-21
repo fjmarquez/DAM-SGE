90c71e931a24c8d115098222cca5e7f4b18de950
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2021-01-21 13:42:18 +0100

    Unidad 13 ejercicios comenzados

---
+++ 11SignalRChat/11SignalRChat/obj/Release/Package/PackageTmp/Scripts/ChatMessage.js
@@ -0,0 +1,6 @@
+﻿class ChatMessage {
+    constructor(username, message) {
+        this.username = username;
+        this.message = message;
+    }
+}
---
+++ 13AJAXEjercicios/13AJAX1/Client/script.js
@@ -0,0 +1,43 @@
+﻿window.onload = function () {
+
+    var btn = document.getElementById("btnHolaMundo");
+
+    btn.onclick = function (e) {
+        llamadaAjax();
+    };
+
+    //btn.addEventListener("click", llamadaAjax);
+
+}
+
+
+
+function llamadaAjax() {
+    //alert("Hola Mundo");
+
+    var llamadaAjax = new XMLHttpRequest();
+
+    llamadaAjax.open("GET", "http://localhost:61375/server/holaMundo.html");
+
+    llamadaAjax.onreadystatechange = function () {
+
+        if (llamadaAjax.readyState < 4) {
+
+        }
+        else {
+
+            if (llamadaAjax.readyState == 4 && llamadaAjax.status == 200) {
+
+                var response = llamadaAjax.responseText;
+
+                document.getElementById("respuesta").innerHTML = response;
+
+            }
+
+        }
+
+    };
+
+    llamadaAjax.send();
+
+}
